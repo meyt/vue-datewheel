b0f7b6ad22c1ce73aa94ebbb013d77a49930e76a
meyt <pasd3000@gmail.com>
2023-05-15 03:17:25 +0330

    Fix missed locales

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteStaticCopy({
+      targets: [
+        { src: 'lib/locales', dest: './' },
+      ]
+    })
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'lib/index.js'),
